234143c35b50deafb906b673132e1b0b2c6149de
maxim190404 <maksipirami@gmail.com>
2025-05-16 11:03:28 +0300

    Update admin.py

---
+++ api_final_yatube/yatube_api/posts/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from .models import Group, Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "text",
+        "pub_date",
+        "author",
+        "group",
+        "image",
+    )
+    search_fields = ("author", "group")
+    list_filter = ("pub_date", "author", "group")
+    list_editable = ("group",)
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "slug",
+        "description",
+    )
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("slug",)
+    list_filter = ("title",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "author", "text", "created")
